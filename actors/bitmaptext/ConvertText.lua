13a723cae17a82b1686b397298936ff8cbde5d7a
JoseVarelaP <JoseVarelaP@users.noreply.github.com>
2023-01-29 00:56:03 +0000

    deploy: 689c91119fb0adb9e63c2bf1f23b791d754f8cf1

--- actors/bitmaptext/ConvertText.lua
+++ actors/bitmaptext/ConvertText.lua
@@ -5,32 +5,25 @@ local function ConvertText( child )
 	local charindex = 0
 	-- Here we'll store the  all the possible matches to color.
 	local ColoringProcess = {}
-	-- First we'll look into all instances where there are words.
-	for k in string.gmatch( str, "(%g+)" ) do
-		-- Now look for matches where they're enclosed in square brackets.
-		for m in string.gmatch( k,"%[(.-)%]" ) do
-			-- If it does, insert the entry into the ColoringProcess table, which includes the following:
-			-- 1 - The number index of where this match occurs
-			-- 2 - The attributes to apply.
-			-- 2.1 -- Diffuse, how to color this section.
-			-- 2.2 -- Length, how long is this section.
-			ColoringProcess[#ColoringProcess+1] = {
-				Start = charindex > 1 and charindex+1 or 0,
-				Attr = {
-					Diffuse = Color.Red,
-					Length = m:len()
-				}
+
+	-- Let's use gmatch to get all matches where a string is enclosed on square brackets.
+	-- A trick being used here are the empty parenthesis, as gmatch doesn't provide a general index value
+	-- when there's a proper match found. However, if you include parenthesis before or after a successful match,
+	-- you'll get the index value of the the start and the end of the match relative to the entire string, which is 
+	-- useful to get the position to apply each attribute.
+	for p1,m,p2 in str:gmatch("()(%[.-%])()" ) do
+		ColoringProcess[#ColoringProcess+1] = {
+			-- The attributes need the value to be 0-indexed, hence the -1, as Lua provides all results starting with 1.
+			Start = p1-1,
+			Attr = {
+				Diffuse = Color.Red,
+				Length = m:len()
 			}
-		end
-		-- Now that we're done with that loop, regardless if we got a match or now, increase the starting index
-		-- to prevent duplicates.
-		charindex = charindex + k:len()
+		}
 	end
 	
-	-- We're done, get rid of the brackets themselves.
-	child:settext( str:gsub( "%[","" ):gsub("%]","") )
+	-- We're done. Apply the resulting table's atributes to the text.
 	for k,v in pairs( ColoringProcess ) do
-		-- Apply the resulting table's atributes to the text.
 		child:AddAttribute( v.Start, v.Attr )
 	end
 end
